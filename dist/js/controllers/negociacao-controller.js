b0c600e7509dbb62a7d41a042dc3ac8d2a051cb0
Julio Cesar Perroni <julio_veia@hotmail.com>
2025-01-09 19:25:23 -0300

    Implementados primeiros métodos

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,26 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limpaFormulario();
+    }
+    criaNegociacao() {
+        const ex = /-/g;
+        const data = new Date(this.inputData.value.replace(ex, ","));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(data, quantidade, valor);
+    }
+    limpaFormulario() {
+        this.inputData.value = "";
+        this.inputQuantidade.value = "";
+        this.inputValor.value = "";
+        this.inputData.focus();
+    }
+}
